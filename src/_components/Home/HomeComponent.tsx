400f66d94e252c9f7e7e665994e2a511b7845d0b
rutvik-rana06 <rutvik.rana@techholding.co>
2024-06-27 11:16:15 +0530

    designed home component

---
+++ src/_components/Home/HomeComponent.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+import LampDemo, { LampContainer } from '../ui/lamp';
+
+const HomeComponent = () => {
+  return (
+    <>
+      <LampDemo />
+    </>
+  );
+};
+
+export default HomeComponent;
